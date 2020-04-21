6dbb551febcb8aca96ee84ed4bb003c9ad8374d4
A2020-ch <59454250+A2020-ch@users.noreply.github.com>
2020-04-20 20:53:58 -0500

    Update ebay.java

--- ebay.java
+++ ebay.java
@@ -1,5 +1,6 @@
 package prueba2;
 
+import jdk.internal.instrumentation.TypeMapping;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +9,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import javax.print.DocFlavor;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class probando {
 
@@ -16,60 +20,49 @@ public class probando {
 
         System.setProperty("webdriver.chrome.driver", "D:\\drivers\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         Actions actions = new Actions(driver);
-        driver.get("https://pe.ebay.com/b/Under-10/bn_7001623770");
+        driver.navigate().to("https://pe.ebay.com/b/Under-10/bn_7001623770");
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
 
 
-        driver.findElement(By.id("gh-shop-ei")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.cssSelector("#gh-sbc > tbody > tr > td:nth-child(1) > ul:nth-child(6) > li:nth-child(4) > a")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.linkText("Calzado para hombres")).click();
-        Thread.sleep(4000);
+        driver.findElement(By.cssSelector("#gh-ac")).sendKeys("shoes");
+        driver.findElement(By.cssSelector("#gh-ac")).sendKeys(Keys.ENTER);;
+        driver.findElement(By.cssSelector("#s0-13-11-0-1-2-6-0-2\\[1\\]-11\\[1\\] > div > input")).sendKeys("pum");
+        driver.findElement(By.xpath("//input[@aria-label = 'PUMA']")).click();
         WebElement talla = driver.findElement(By.cssSelector("input[aria-label = '10']"));
         talla.click();
-        Thread.sleep(4000);
-        driver.findElement(By.cssSelector("button[aria-label = 'Recuadro de especificaciones de búsqueda de Marca']")).click();
-        Thread.sleep(3000);
-        WebElement puma = driver.findElement(By.cssSelector("#c3-subPanel-Brand_PUMA_cbx"));
-        puma.click();
-        driver.findElement(By.cssSelector("button[aria-label = 'Aplicar']")).click();
-        Thread.sleep(2000);
-        WebElement select = driver.findElement(By.cssSelector("#s0-25-9-0-1\\[0\\]-0-1-6-5-35 > button > div > svg > use"));
-        actions.moveToElement(select).perform();
+        driver.findElement(By.cssSelector("#s0-13-11-5-1\\[0\\]-42-0 > button")).click();
         driver.findElement(By.linkText("Precio + Envío: más bajo primero")).click();
         Thread.sleep(3000);
+        WebElement result = driver.findElement(By.cssSelector("h1.srp-controls__count-heading"));
+        result.getText();
+        System.out.println(result.getText());
+        List <String> productos = new ArrayList();
+        productos.add(driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[1]/div/div[2]/a/h3")).getText()+ "\n");
+        productos.add(driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[2]/div/div[2]/a/h3")).getText()+ "\n");
+        productos.add(driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[3]/div/div[2]/a/h3")).getText()+ "\n");
+        productos.add(driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[4]/div/div[2]/a/h3")).getText()+ "\n");
+        productos.add(driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[5]/div/div[2]/a/h3")).getText()+ "\n");
+        Collections.sort(productos);
+        System.out.println("ordenando por nombre de producto: ");
+        System.out.println(productos);
+        List <String>  precio = new ArrayList();
+        precio.add((driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[1]/div/div[2]/div[3]/div[1]/span")).getText()) + "\n");
+        precio.add((driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[2]/div/div[2]/div[3]/div[1]/span")).getText())+ "\n");
+        precio.add((driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[3]/div/div[2]/div[3]/div[1]/span")).getText())+ "\n");
+        precio.add((driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[4]/div/div[2]/div[3]/div[1]/span")).getText())+ "\n");
+        precio.add((driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[5]/div/div[2]/div[3]/div[1]/span")).getText())+ "\n");
 
-        String[] page = {"Puma Adultos Unisex Epic Flip v2 De Verano Playa Sandalias diapositivas Ojotas Negro..", "Puma Adultos Unisex Epic Flip v2 De Verano Playa Sandalias diapositivas Ojotas Blancas..", "Puma Adultos Unisex POPCAT Verano Playa Deportes Correa Diapositiva Sandalias Zapatos Blanco..", "Puma Adultos Unisex leadcat Verano Playa Deportes Correa Diapositiva Sandalias Zapatos Negro.", "Puma Shoestring Rihanna Jelly Slip On Deslizadores Para Hombres Sandalias Ojotas 362465 05 D44"};
-
-
-        System.out.println("Los zapatos seleccionados son:");
-        for (String d : page) {
-
-            driver.findElement(By.linkText(d)).click();
-            Thread.sleep(2000);
-            driver.findElement(By.linkText("Agregar al carro de compras")).click();
-            System.out.println(d);
-            Thread.sleep(2000);
-            driver.get("https://pe.ebay.com/b/PUMA-Shoes-US-Size-10-for-Men/93427/bn_111288631?listingOnly=1&rt=nc&_dmd=1&_sop=15");
-        }
-
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("#gh-minicart-hover > div > a.gh-eb-li-a.gh-cart-count-1 > svg > use")).click();
-        System.out.println("Los zapatos seleccionados son:");
-        for  (String d : page) {
-            System.out.println(d);
-        }
-        
-
-        }
-
-
-
+        Collections.sort(precio);
+        System.out.println("ordenando por precio descendente: ");
+        System.out.println(precio);
 
 
 
     }
+
+
+
 }
